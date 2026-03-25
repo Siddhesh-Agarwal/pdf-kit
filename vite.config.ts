bc99262a21f24707247263007535a38852ddc417
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2026-03-25 15:15:16 +0530

    feat: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import babel from "@rolldown/plugin-babel";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+	plugins: [
+		tanstackRouter({
+			target: "react",
+			autoCodeSplitting: true,
+		}),
+		tailwindcss(),
+		react(),
+		babel({ presets: [reactCompilerPreset()] }),
+	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+});
